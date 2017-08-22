17489871be3d57f58f45a42ad64dd511f2168182
Aman Upadhyay <31204643+aman-upadhyay@users.noreply.github.com>
2017-08-22 20:34:36 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main(){
+
+int a;
+int b;
+int s;
+cout<< "Enter First number";
+cin>>a;
+cout<< "Enter Second Number";
+cin>>b;
+s = a+b;
+cout<< "Addition :"<<s;
+}
---
+++ 3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+main(){
+
+float a,e,r;
+int b,s,m,d,q;
+cout<< "Enter First number";
+cin>>a;
+cout<< "Enter Second Number";
+cin>>b;
+s = a+b;
+cout<< "Addition :"<<s;
+q = a-b;
+cout<< " subtraction ;"<<q;
+m = a*b;
+cout<< "Multiplication is :"<<m;
+d = a/b;
+cout<< "division :"<<d;
+e = a/b;
+r = (e-d)*b;
+cout<< "remainder :"<<r; 
+//I know how to use reminder opretor %,but still I wanted try without using it 
+}
