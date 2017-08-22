d0176124d1635d6c9e7a5e22014bd3f7fd5c053a
Aman Upadhyay <31204643+aman-upadhyay@users.noreply.github.com>
2017-08-22 20:49:15 +0530

    Add files via upload

---
+++ lab1_question4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main(){
+
+int a;
+int b;
+int s;
+cout<< "Enter Length of Rectangle";
+cin>>a;
+cout<< "Enter Breadth of Rectangle";
+cin>>b;
+s = 2*(a+b);
+cout<< "Perimeter is :"<<s;
+}
