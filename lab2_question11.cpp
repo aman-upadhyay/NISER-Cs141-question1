d5c28a91c377bfdd7a74fcb1851cb0b4476b14f2
Aman Upadhyay <31204643+aman-upadhyay@users.noreply.github.com>
2017-08-29 16:22:24 +0530

    Add files via upload

---
+++ lab2_question11.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+ {
+
+float p,r,t,s;
+cout<< "Enter principle amount";
+cin>>p;
+cout<< "Enter rate ";
+cin>>r;
+cout<< "Enter time";
+cin>>t;
+
+s = (p*r*t)/100;
+cout<< "Simple Intrest is  "<<s;
+
+return 0;
+}
+
