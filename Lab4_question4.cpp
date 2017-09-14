fe91af8dc0796861c3fd93164c65e3386c6891b6
Aman Upadhyay <31204643+aman-upadhyay@users.noreply.github.com>
2017-09-14 23:34:54 +0530

    Update Lab4_question4.cpp

--- Lab4_question4.cpp
+++ Lab4_question4.cpp
@@ -3,16 +3,14 @@ using namespace std;
   
      int even_odd (int a)
        {
-          float b,c;
-         c = (b/2)-(a/2);
-          if (c==0)
-             cout<<"no is even";
+          if (a%2==0)
+             cout<<"The number is even";
           else 
-             cout<<"no is odd";
+             cout<<"The number is odd";
         }
       int main ()
        { int a;
-            cout<<"Enter a no to check even odd";
+            cout<<"Enter a number to check even odd";
             cin>>a;
             even_odd(a);
 }
