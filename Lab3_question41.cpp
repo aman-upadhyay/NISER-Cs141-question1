9cde9c637ba8f4b5ac3ba18998d000502862712e
Aman Upadhyay <31204643+aman-upadhyay@users.noreply.github.com>
2017-09-14 23:17:21 +0530

    Update Lab3_question41.cpp

--- Lab3_question41.cpp
+++ Lab3_question41.cpp
@@ -4,13 +4,13 @@ using namespace std;
   int main ()
   {
       char a;
-     
+     int i;
     a=0;
       while(i<127)
       { 
+        a=i;
         cout<<"For ASCII "<<a<<" has its value in "<<i<<"\n";
-        i++;
-        a=i; 
+        i++; 
       }
      return 0;
   }
